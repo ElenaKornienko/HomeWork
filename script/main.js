37b971a86ed27abb92aef85d30eb3219d53d86a5
Elena Kornienko <kornelena2005@gmail.com>
2018-02-12 17:05:27 +0200

    homeWork7

--- script/main.js
+++ script/main.js
@@ -1,23 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-	let trap = document.querySelector('.block-trap');
+	let cube = document.getElementById('cube');
+	let platform = document.getElementById('platform');
+    let step = 1;
+    let screenH = window.innerHeight;
+ 	let cubeH = cube.clientHeight;
+ 	let platformH = platform.clientHeight;
+ 	let conditionH = screenH - cubeH;
+ 	let posTop = screenH - platformH;
+	
+    cube.style.top = 0 + 'px';
+    cube.style.left = 0 + 'px';
+    platform.style.left = 0 + 'px';
+    platform.style.top = posTop + 'px';
 
-	trap.addEventListener('mousemove', function() {
-		X = event.pageX;
-		trap.style.left = X + 'px';
-	})
-	let one = document.querySelector('.block1');
-	let y=0;
-	let start = setInterval (move, 5);
-	function move() {
-		if (y == screen.availHeight-32) {
-			clearInterval (start);
-		} else {
-			y++;
-			one.style.top = y +'px';
-		}
-	}	
-})
+    let moveCube = setInterval (() => {
+    	let topCube = cube.style.top.slice(0,-2);
+        topCube = parseInt(topCube);
+        topCube = topCube + step;
+    	cube.style.top = topCube + 'px';
+    	let leftCube = parseInt(cube.style.left.slice(0,-2));
+    	let leftPlatform = parseInt(platform.style.left.slice(0,-2));
+        let topPlatform = parseInt(platform.style.top.slice(0,-2));
+        //console.log(topCube, leftCube, topPlatform, leftPlatform);	
+        if(topCube === conditionH) {
+          cube.style.top = 0;
+        }
+        if (topCube === topPlatform && leftCube === leftPlatform) {
+        	alert('Victory!');
+        }
+    }, 20);
 
-
-
+    document.addEventListener('mousemove', function(e) {
+        //console.log(e.clientX, e.clientY);    
+        platform.style.left = e.clientX + 'px';
+    });
+})    
 
